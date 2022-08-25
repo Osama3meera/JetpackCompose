6030879a891131754f6d79fea201bb11f17a9e65
admin <admin@admins-MacBook-Pro.local>
2022-08-25 09:36:02 +0300

    push test

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -48,7 +48,6 @@ fun CreateBizCard() {
                 .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 4.dp
-        //
         ) {
 
         }
